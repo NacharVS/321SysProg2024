3f5e4820915c15ebb70a9c5599dabcf3368e16a9
Danil Gareev <gecktore00@mail.ru>
2024-01-13 15:26:03 +0300

    nakonec

---
+++ StrategyUnits/Healer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Healer : Unit
+    {
+        private int _heal;
+        private int _mana;
+
+        public int Heal { 
+            get { return _heal; }
+            set { _heal = value; }
+        }
+        public int Mana
+        {
+            get { return _mana;  }
+            set { _mana = value; }
+        }
+        
+        public Healer() : base(50, "Healer")
+        {
+            _heal = 1;
+            _mana = 2;
+        }
+        public void AddHeal(Unit unit)
+        {
+            while (unit.Health < unit.MaxHealth && _mana>0)
+            {
+                if (unit.Health >= unit.MaxHealth)
+                {
+                    Console.WriteLine("Юнит вылечен!");
+                    unit.Health = unit.MaxHealth;
+                }
+                else
+                {
+                    unit.Health += _heal;
+                    _mana -= _heal * 2;
+                }
+            }
+            Console.WriteLine("Юнит здоров либо нет маны!");
+
+        }
+    }
+}
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,6 +3,7 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Healer healer1 = new Healer();
 
 ps1.ShowInfo();
 footman2.ShowInfo();
@@ -10,4 +11,5 @@ footman.InflictDamage(ps1);
 footman.InflictDamage(footman2);
 ps1.ShowInfo();
 footman2.ShowInfo();
-
+healer1.AddHeal(ps1);
+ps1.ShowInfo();
