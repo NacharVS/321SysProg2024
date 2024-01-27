c5ea48d63fed53ba035fa8e5d6531d16c8ae8f67
Danil Gareev <gecktore00@mail.ru>
2024-01-27 14:29:11 +0300

    IArcher

--- StrategyUnits/Human/Archer.cs
+++ StrategyUnits/Human/Archer.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StrategyUnits.Interfaces;
 using StrategyUnits.Type;
 
 namespace StrategyUnits.Human
 {
-    internal class Archer : militaryUnits
+    internal class Archer : IHealthControll, IBattleUnit, IComboAttack
     {
-        public Archer() : base(60, "Archer", 1, 3, 5)
+        public Archer()
         {
+            Health = 50;
+            Damage = 10;
+            Combo = 3;
+        }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public int Combo { get; set; }
+        public void Attack (IHealthControll unit)
+        {
+            int _doneCombo = 0;
+            while(_doneCombo < Combo)
+            {
+                unit.TakeDamage(Damage);
+                _doneCombo++;
+            }
+        }
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
         }
     }
 }
