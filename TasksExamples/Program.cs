f0d138fdfb43e03cfc45d48267d99fbafb234ebd
r3dinc <r3dinc@yandex.ru>
2024-02-14 14:10:39 +0300

    tasks start

---
+++ TasksExamples/Program.cs
@@ -0,0 +1,17 @@
+﻿Task task = new Task(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Thread.Sleep(500);
+        Console.WriteLine($"task {i}");
+    }
+});
+
+task.Start();
+
+for (int i = 0; i < 5; i++)
+{
+    Thread.Sleep(500);
+    Console.WriteLine($"main {i}");
+}
+task.Wait();
